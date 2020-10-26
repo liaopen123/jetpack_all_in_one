77689b9ef035ff9a2fd8adf539f816a9a440e13d
liaopen123 <467969@qq.com>
2020-10-27 00:23:29 +0800

    Initial commit

---
+++ app/src/main/java/com/lph/jetpack_all_in_one/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lph.jetpack_all_in_one
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
